8e7d5c552c497ed30fcf92d9cf85cf50dd63cfd5
Matteo Baglini <matteo.baglini@gmail.com>
2018-12-20 22:43:45 +0100

    INSTALL: leave only link to {rc, dot, config}files

--- install.sh
+++ install.sh
@@ -7,45 +7,7 @@ rm -f ~/.hushlogin && ln -sf "$DOT_FILES_DIR/hushlogin" ~/.hushlogin
 rm -f ~/.gitconfig && ln -sf "$DOT_FILES_DIR/gitconfig" ~/.gitconfig
 rm -rf ~/.vim && ln -sf "$DOT_FILES_DIR/vim" ~/.vim
 rm -f ~/.vimrc && ln -sf "$DOT_FILES_DIR/vimrc" ~/.vimrc
-
-echo "Install vim-plug and other plugins"
-if [ ! -f $DOT_FILES_DIR/vim/autoload/plug.vim ]; then
-    curl -fLo $DOT_FILES_DIR/vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-else
-    vim +PlugUpgrade +qall &>/dev/null
-fi
-vim +PlugClean! +qall &>/dev/null
-vim +PlugUpdate +qall &>/dev/null
-
-echo "Install powerline fonts"
-if [ ! -d ~/powerline-fonts ]; then
-    git clone --recursive --depth 1 \
-        https://github.com/powerline/fonts/ \
-        ~/powerline-fonts
-    cd ~/powerline-fonts
-else
-    cd ~/powerline-fonts
-    git pull
-fi
-./install.sh &>/dev/null
-cd $OLDPWD
-
-echo "Install awesome terminal fonts"
-if [ ! -d ~/awesome-fonts ]; then
-    git clone --recursive --depth 1 \
-        https://github.com/gabrielelana/awesome-terminal-fonts/ \
-        ~/awesome-fonts
-    cd ~/awesome-fonts
-else
-    cd ~/awesome-fonts
-    git pull
-fi
-./install.sh &>/dev/null
-cd $OLDPWD
-
-if [ -n "$DISPLAY" ]; then
-    echo "Configure Gnome Terminal profile"
-    dconf reset -f "/org/gnome/terminal/"
-    cat gnome-terminal.ini | dconf load "/org/gnome/terminal/"
-fi
+rm -f ~/.config/Code/User/settings.json && ln -sf "$DOT_FILES_DIR/vscode/settings.json" ~/.config/Code/User/settings.json
+rm -f ~/.config/Code/User/locale.json && ln -sf "$DOT_FILES_DIR/vscode/locale.json" ~/.config/Code/User/locale.json
+rm -f ~/.config/Code/User/vsicons.settings.json && ln -sf "$DOT_FILES_DIR/vscode/vsicons.settings.json" ~/.config/Code/User/vsicons.settings.json
+rm -f ~/.config/Code/User/keybindings.json && ln -sf "$DOT_FILES_DIR/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
