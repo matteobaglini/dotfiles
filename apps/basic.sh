6b7c4027b954dbf333f54815f50a53621e2ce399
Matteo Baglini <matteo.baglini@gmail.com>
2020-11-06 23:23:39 +0100

    more apps

--- apps/basic.sh
+++ apps/basic.sh
@@ -8,7 +8,8 @@ sudo apt -q -y install \
     build-essential autoconf linux-kernel-headers \
     git curl wget tree whois unzip gpg htop \
     jq httpie bat fd-find libsecret-1-0 \
-    libsecret-1-dev gnome-tweaks
+    libsecret-1-dev gnome-tweaks \
+    python3-dev python3-pip python3-setuptools
 
 echo ">>>> Cleanup"
 sudo apt -q -y autoremove --purge
--- apps/misc.sh
+++ apps/misc.sh
@@ -4,3 +4,6 @@ sudo snap install slack --classic
 sudo snap install postman
 sudo snap install gimp
 sudo snap install vlc
+
+sudo pip3 install thefuck
+npm install -g tldr
