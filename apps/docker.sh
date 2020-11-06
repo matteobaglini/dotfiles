da8010c189ca4aefba39d60b777b4ba457cd4e57
Matteo Baglini <matteo.baglini@gmail.com>
2020-11-06 22:25:00 +0100

    fix docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 echo ">>>> Install Docker & tools"
-sudo -i <<HEREDOC
-    if ! docker version &>/dev/null; then
-        wget -qO- https://get.docker.com/ | bash
-        sudo usermod -aG docker matteo
-    fi
-    if ! docker-compose --version &>/dev/null; then
-        curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` \
-                > /usr/local/bin/docker-compose
-        chmod +x /usr/local/bin/docker-compose
-    fi
-HEREDOC
+if ! docker version &>/dev/null; then
+    wget -qO- https://get.docker.com/ | bash
+    sudo usermod -aG docker "$USER"
+    newgrp docker
+fi
+if ! docker-compose --version &>/dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
 
-echo ">>>> That's all, rock on!"
