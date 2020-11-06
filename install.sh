0f8c559d06973455d416aad95fb2690c82cfe3d2
Matteo Baglini <matteo.baglini@gmail.com>
2020-11-06 14:14:12 +0100

    add bash files

--- install.sh
+++ install.sh
@@ -5,8 +5,20 @@ DOT_FILES_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo ">> Make links between $DOT_FILES_DIR and $HOME"
 rm -f ~/.hushlogin && ln -sf "$DOT_FILES_DIR/hushlogin" ~/.hushlogin
 rm -f ~/.gitconfig && ln -sf "$DOT_FILES_DIR/gitconfig" ~/.gitconfig
+
 rm -rf ~/.vim && ln -sf "$DOT_FILES_DIR/vim" ~/.vim
 rm -f ~/.vimrc && ln -sf "$DOT_FILES_DIR/vimrc" ~/.vimrc
+
+rm -f ~/.bashrc && ln -sf "$DOT_FILES_DIR/bash/bashrc" ~/.bashrc
+rm -f ~/.bash_aliases && ln -sf "$DOT_FILES_DIR/bash/bash_aliases" ~/.bash_aliases
+rm -f ~/.bash_exports && ln -sf "$DOT_FILES_DIR/bash/bash_exports" ~/.bash_exports
+rm -f ~/.bash_logout && ln -sf "$DOT_FILES_DIR/bash/bash_logout" ~/.bash_logout
+rm -f ~/.bash_options && ln -sf "$DOT_FILES_DIR/bash/bash_options" ~/.bash_options
+rm -f ~/.bash_profile && ln -sf "$DOT_FILES_DIR/bash/bash_profile" ~/.bash_profile
+rm -f ~/.bash_prompt && ln -sf "$DOT_FILES_DIR/bash/bash_prompt" ~/.bash_prompt
+rm -f ~/.bash_functions && ln -sf "$DOT_FILES_DIR/bash/bash_functions" ~/.bash_functions
+
+mkdir -p ~/.config/Code/User
 rm -f ~/.config/Code/User/settings.json && ln -sf "$DOT_FILES_DIR/vscode/settings.json" ~/.config/Code/User/settings.json
 rm -f ~/.config/Code/User/locale.json && ln -sf "$DOT_FILES_DIR/vscode/locale.json" ~/.config/Code/User/locale.json
 rm -f ~/.config/Code/User/vsicons.settings.json && ln -sf "$DOT_FILES_DIR/vscode/vsicons.settings.json" ~/.config/Code/User/vsicons.settings.json
