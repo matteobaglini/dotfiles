7e7186e6e6c40240ab85099fcecc619adbae0d3b
Matteo Baglini <matteo.baglini@gmail.com>
2020-11-07 00:01:05 +0100

    solarized terminal and more stuff

--- apps/basic.sh
+++ apps/basic.sh
@@ -8,7 +8,7 @@ sudo apt -q -y install \
     build-essential autoconf linux-kernel-headers \
     git curl wget tree whois unzip gpg htop \
     jq httpie bat fd-find libsecret-1-0 \
-    libsecret-1-dev gnome-tweaks \
+    libsecret-1-dev gnome-tweaks dconf-cli \
     python3-dev python3-pip python3-setuptools
 
 echo ">>>> Cleanup"
---
+++ terminal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/aruhier/gnome-terminal-colors-solarized.git ~/gnome-terminal-colors-solarized
+cd ~/gnome-terminal-colors-solarized
+source set_light.sh
