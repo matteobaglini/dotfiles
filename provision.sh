28658bdbd8d72bc02a03bf83fd9a8c1acd2732d1
Matteo Baglini <matteo.baglini@gmail.com>
2020-11-07 00:01:05 +0100

    solarized terminal and more stuff

--- provision.sh
+++ provision.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 source apps/basic.sh
-source apps/vim.sh
-source apps/chrome.sh
 source apps/asdf.sh
+source apps/chrome.sh
 source apps/docker.sh
+source apps/editors.sh
+source apps/misc.sh
 
 echo ">>>> That's all, rock on!"
