1c4eb137d735316e93cbf02e084ca4ae250022f5
Matteo Baglini <matteo.baglini@gmail.com>
2021-02-09 09:42:06 +0100

    misc: install toggle command line

--- apps/misc.sh
+++ apps/misc.sh
@@ -8,6 +8,8 @@ sudo snap install scrcpy
 
 sudo pip3 install thefuck
 sudo pip3 install envdir
+sudo pip3 install togglCli
+
 npm install -g tldr
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
