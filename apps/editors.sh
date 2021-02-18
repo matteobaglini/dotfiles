d5258296757fc3b6dc25bd827f7c718cd42bff19
Matteo Baglini <matteo.baglini@gmail.com>
2021-02-18 17:16:44 +0100

    apps: install datagrip and webstorm via snap

--- apps/editors.sh
+++ apps/editors.sh
@@ -8,4 +8,6 @@ sudo apt install -q -y vim-gtk3
 
 sudo snap install code --classic
 sudo snap install rider --classic
+sudo snap install datagrip --classic
+sudo snap install webstorm --classic
 sudo snap install intellij-idea-community --classic
