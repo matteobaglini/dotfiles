1fa84f248898978c036ae804d965f102b5820ad3
Matteo Baglini <matteo.baglini@gmail.com>
2022-03-31 15:31:05 +0200

    bash: install starship and init only if present

--- apps/misc.sh
+++ apps/misc.sh
@@ -15,3 +15,19 @@ npm install -g tldr
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+echo ">>>> Install Google Chrome"
+sudo -iu matteo <<HEREDOC
+    if [ ! -f /etc/apt/sources.list.d/google.list ]; then
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+        sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+        sudo apt-get update -q -y
+        sudo apt-get install -q -y google-chrome-stable
+    fi
+HEREDOC
+
+echo ">>>> Install FireFox"
+sudo apt install -q -y firefox
+
+echo ">>>> Install Starship"
+curl -fsSL https://starship.rs/install.sh | bash
--- provision.sh
+++ provision.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-source apps/basic.sh
-source apps/git.sh
-source apps/python.sh
-source apps/dotnet.sh
-source apps/asdf.sh
-source apps/browsers.sh
-source apps/docker.sh
-source apps/editors.sh
-source apps/misc.sh
-source apps/cleanup.sh
+# Load the shell files
+for file in ~/.{basic,git,python,dotnet,asdf,docker,editors,misc,cleanup}; do
+    [ -r "$file" ] && [ -f "$file" ] && source "$file";
+done;
+unset file;
 
 echo ">>>> That's all, rock on!"
