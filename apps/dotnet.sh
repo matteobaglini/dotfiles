4bdeb27ac99354c6c67671b780eafc8d679a0df6
Matteo Baglini <matteo.baglini@gmail.com>
2022-03-31 15:38:15 +0200

    minor

--- apps/dotnet.sh
+++ apps/dotnet.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+echo ">>>> Install .NET packeges"
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-
-sudo apt-get update
-sudo apt-get install -q -y apt-transport-https
-
-sudo apt-get update
-sudo apt-get install -q -y dotnet-sdk-2.1 dotnet-sdk-3.1 dotnet-sdk-5.0 dotnet-sdk-6.0
-
-# Cleanup
 rm packages-microsoft-prod.deb
+
+sudo apt-get -q -y update
+sudo apt-get install -q -y \
+	apt-transport-https \ 
+	dotnet-sdk-2.1 \
+	dotnet-sdk-3.1 \
+	dotnet-sdk-5.0 \
+	dotnet-sdk-6.0
--- apps/python.sh
+++ apps/python.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo ">>>> Update packages"
-sudo apt -q -y update
-
 echo ">>>> Install Python packeges"
+sudo apt -q -y update
 sudo apt -q -y install \
     python3-dev python3-pip python3-setuptools
