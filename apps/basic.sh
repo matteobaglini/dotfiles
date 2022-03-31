1b706b2f8a6020e15fc2a1a41906aa831ca8f31c
Matteo Baglini <matteo.baglini@gmail.com>
2022-03-31 12:31:48 +0200

    apps: remove lazygit

--- apps/basic.sh
+++ apps/basic.sh
@@ -11,9 +11,5 @@ sudo apt -q -y install \
     libsecret-1-dev gnome-tweaks dconf-cli \
     python3-dev python3-pip python3-setuptools
 
-sudo add-apt-repository ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install -yq lazygit
-
 echo ">>>> Cleanup"
 sudo apt -q -y autoremove --purge
