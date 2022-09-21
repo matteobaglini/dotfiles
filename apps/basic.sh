2157d6a69258bfbec0515ffb284d9aace0f33577
Matteo Baglini <matteo.baglini@gmail.com>
2022-09-21 18:14:48 +0200

    provisioning: install and use dra utility

--- apps/basic.sh
+++ apps/basic.sh
@@ -9,4 +9,13 @@ echo ">>>> Install basic packeges"
 sudo apt -q -y install \
     build-essential autoconf linux-kernel-headers \
     curl wget tree whois unzip gpg htop \
-    jq httpie bat fd-find gnome-tweaks dconf-cli
+    jq httpie bat fd-find gnome-tweaks dconf-cli \
+    fonts-firacode
+
+
+echo ">>>> Install basic packeges"
+curl -fsSL -o dra.deb \
+    https://github.com/devmatteini/dra/releases/download/0.3.8/dra_0.3.8_amd64.deb
+sudo dpkg -i ./dra.deb
+rm ./dra.deb
+dra completion bash >"$HOME"/.local/share/bash-completion/completions/dra
