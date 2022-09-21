d129b390ab1a5c6c740aae6ca881e97889505c13
Matteo Baglini <matteo.baglini@gmail.com>
2022-09-21 19:28:21 +0200

    apps-misc: remove tldr

--- apps/misc.sh
+++ apps/misc.sh
@@ -11,8 +11,6 @@ sudo snap install scrcpy
 sudo pip3 install envdir
 sudo pip3 install togglCli
 
-npm install -g tldr
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
