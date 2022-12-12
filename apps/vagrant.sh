3f0f737d3973dcd88cdc8c3d73d71fad663d660d
Matteo Baglini <matteo.baglini@gmail.com>
2022-12-12 12:50:43 +0100

    install vagrant

---
+++ apps/vagrant.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ">>>> Vagrant"
+
+sudo apt update && sudo apt install -q -y  virtualbox
+
+sudo mkdir -p /usr/share/keyrings
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update && sudo apt install -q -y vagrant
